ef2af9a384f0a2c16e60da30053effbeea5c2b45
Manuel Blechschmidt <manuel.blechschmidt@gmx.de>
2018-05-12 22:41:22 +0200

    Added url for Vector

--- src/incentergy/ol/source/Vector.js
+++ src/incentergy/ol/source/Vector.js
@@ -8,11 +8,32 @@ sap.ui.define(['ol', './Source'],
         var Vector = Source.extend("incentergy.ol.source.Vector", {
             metadata: {
                 library: "incentergy.ol",
+                properties: {
+                    "url": {
+                        /**
+                         * The url from where the Tiles should
+                         */
+                        type: "string",
+                        group: "Misc",
+                        defaultValue: null
+                    },
+                    "format": {
+                        type: "string",
+                        group: "Misc",
+                        defaultValue: "GeoJSON"
+                    },
+                    "crs": {
+                        type: "string",
+                        group: "Misc",
+                        defaultValue: "EPSG:3857"
+                    }
+                },
                 aggregations: {
                     "features": {
                         type: "incentergy.ol.Feature",
                         multiple: true,
-                        singularName: "feature"
+                        singularName: "feature",
+                        bindable: true
                     }
                 },
                 defaultAggregation: "features",
@@ -24,9 +45,34 @@ sap.ui.define(['ol', './Source'],
                 Source.apply(this, arguments);
                 this._source = new ol.source.Vector();
                 var me = this;
-                this._source.on("addfeature", function(){
+                this._source.on("addfeature", function() {
                     me.fireAddfeature();
                 });
+            },
+            setUrl: function(sUrl) {
+                var me = this;
+                var retVal = this.setProperty("url", sUrl);
+                try {
+                    if (this._source) {
+                        this._source.setUrl(me._getUrlFunction());
+                    }
+                } catch (e) {
+                    jQuery.sap.log.error(e);
+                }
+
+                return retVal;
+            },
+            setFormat: function(format) {
+                this._source.setFormat(new ol.format[format]());
+                return this.setProperty("format", format);
+            },
+            _getUrlFunction: function() {
+                var me = this;
+                return function(extent) {
+                    return me.getUrl() +
+                        (extent ? 'bbox=' + extent.join(',') + ',' : '') +
+                        '&' + me.getCrs();
+                };
             }
         });
         return Vector;
